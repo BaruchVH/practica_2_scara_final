945b948062ee7f30c6ad5111528ffcf781c43be3
BaruchVH <baruchvh12@gmail.com>
2026-05-30 21:06:16 -0600

    Practica 6 SCARA con evidencias

---
+++ build/scara_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arrgusr/ROS2Dev/ws_sem26_2/scara_bringup
+# Build directory: /home/arrgusr/ROS2Dev/ws_sem26_2/build/scara_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
